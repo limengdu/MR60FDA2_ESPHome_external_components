21c2da93f503775b8b76606e0e82cff231ceaa96
limengdu <37475446+limengdu@users.noreply.github.com>
2024-09-09 11:34:46 +0800

    Fix

--- components/seeed_mr60fda2/seeed_mr60fda2.cpp
+++ components/seeed_mr60fda2/seeed_mr60fda2.cpp
@@ -151,7 +151,7 @@ void MR60FDA2Component::splitFrame(uint8_t buffer) {
         this->current_frame_buf[this->current_frame_len_ - 1] = buffer;
         this->current_frame_locate_++;
         // ESP_LOGD(TAG, "%x", this->current_frame_buf[this->current_frame_len_ - 1]);
-        ESP_LOGD(TAG, "GET CURRENT_FRAME_TYPE: 0x%02x 0x%02x", this->current_frame_buf[this->current_frame_len_ - 2], this->current_frame_buf[this->current_frame_len_ - 1]);
+        // ESP_LOGD(TAG, "GET CURRENT_FRAME_TYPE: 0x%02x 0x%02x", this->current_frame_buf[this->current_frame_len_ - 2], this->current_frame_buf[this->current_frame_len_ - 1]);
       } else {
         // ESP_LOGD(TAG, "CURRENT_FRAME_TYPE NOT FOUND: %x", this->current_frame_type_);
         ESP_LOGD(TAG, "GET HEAD_CKSUM_FRAME: 0x%02x 0x%02x", this->current_frame_buf[this->current_frame_len_ - 2], this->current_frame_buf[this->current_frame_len_ - 1]);
