da3e5815daf8e2eded2cd4324b6a48f317212519
limengdu <37475446+limengdu@users.noreply.github.com>
2024-09-09 15:02:37 +0800

    Finish version 1

--- components/seeed_mr60fda2/binary_sensor.py
+++ components/seeed_mr60fda2/binary_sensor.py
@@ -1,21 +1,15 @@
 import esphome.codegen as cg
 from esphome.components import binary_sensor
 import esphome.config_validation as cv
-from esphome.const import (
-    DEVICE_CLASS_OCCUPANCY,
-    DEVICE_CLASS_EMPTY,
-)
+from esphome.const import DEVICE_CLASS_OCCUPANCY
 from . import CONF_MR60FDA2_ID, MR60FDA2Component
 
 AUTO_LOAD = ["seeed_mr60fda2"]
-CONF_IS_FALL = "is_fall"
+
 CONF_PEOPLE_EXIST = "people_exist"
 
 CONFIG_SCHEMA = {
     cv.GenerateID(CONF_MR60FDA2_ID): cv.use_id(MR60FDA2Component),
-    cv.Optional(CONF_IS_FALL): binary_sensor.binary_sensor_schema(
-        device_class=DEVICE_CLASS_EMPTY, icon="mdi:security"
-    ),
     cv.Optional(CONF_PEOPLE_EXIST): binary_sensor.binary_sensor_schema(
         device_class=DEVICE_CLASS_OCCUPANCY, icon="mdi:motion-sensor"
     ),
@@ -24,9 +18,6 @@ CONFIG_SCHEMA = {
 
 async def to_code(config):
     mr60fda2_component = await cg.get_variable(config[CONF_MR60FDA2_ID])
-    if is_fall_config := config.get(CONF_IS_FALL):
-        sens = await binary_sensor.new_binary_sensor(is_fall_config)
-        cg.add(mr60fda2_component.set_is_fall_binary_sensor(sens))
     if people_exist_config := config.get(CONF_PEOPLE_EXIST):
         sens = await binary_sensor.new_binary_sensor(people_exist_config)
         cg.add(mr60fda2_component.set_people_exist_binary_sensor(sens))
